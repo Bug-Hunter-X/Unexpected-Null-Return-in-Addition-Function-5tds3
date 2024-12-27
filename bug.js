88085ba60a34752489d341feadb9a8f747a5a777
BugHunter-X <askzenai@gmail.com>
2024-12-27 07:50:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return null; //This will cause unexpected behavior
+  }
+  return a + b;
+}
+console.log(foo(1, null)); // Output: null
+console.log(foo(1, 2)); // Output: 3
